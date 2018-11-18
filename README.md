458823622b7eea9d752f3dcdf8ed411d0ac6084e
Bartek Orawiec <Bartek@MacBook-Pro-Bartek.local>
2018-11-18 17:34:13 +0100

    working varsion

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Space Invaders is an arcade game created by Tomohiro Nishikado and released in 1978. It was manufactured and sold by Taito in Japan. Space Invaders is one of the earliest shooting games; the aim is to defeat waves of aliens with a laser to earn as many points as possible.
 
-My version of Space Invaders, based on original one.
+This program is my version of Space Invaders, which is based on the original one.
 
 ## Authors
 
--- master.js
+++ master.js
@@ -1,44 +1,140 @@
-class alien {
+let canvas, c, i1, i2,player,alive, aliens = [], bullets = [];
+
+function start() {
+    canvas = document.getElementById('can');
+    c = canvas.getContext('2d');
+    alienImage = document.getElementById("alien01");
+    playerImage = document.getElementById("player01");
+    rocketImage = document.getElementById("rocket01");
+    backImage = document.getElementById("back");
+    player = new Player(270, 540);
+    addEnemies(4);
+
+    document.addEventListener('keydown', (event) => {
+        const keyName = event.key;
+        if (keyName == " ") {
+            if (bullets.length < 3)
+                bullets.push(new Bullet(player.x + 30, player.y));
+        }
+        else if (keyName == "ArrowLeft") player.moveLeft();
+        else if (keyName == "ArrowRight") player.moveRight();
+    });
+
+    i1 = setInterval(function () {
+        redraw(new State(player, aliens, bullets));
+    }, 80);
+
+    i2 = setInterval(function () {
+        attack(new State(player, aliens, bullets));
+    }, 150);
+}
+
+
+class State {
+    constructor(player, aliens, bullets) {
+        this.aliens = aliens;
+        this.player = player;
+        this.bullets = bullets;
+    }
+}
+
+function addEnemies(level) {
+    if (level > 5 || level < 1) level = 1;
+    for (let y = 0; y < level; y++) {
+        for (let x = 55; x <= 530; x += 110) {
+            aliens.push(new Alien(x, 40 + y * 70));
+        }
+    }
+    alive = aliens.length;
+}
+
+function attack(state){
+    for (a of state.aliens) {
+        a.y += 5;
+        if(a.y > 490 && a.alive) end(0);
+        if(alive <= 0) end(1);
+    }
+}
+
+function end(status){
+    clearInterval(i1);
+    clearInterval(i2);
+    c.font = "30px Arial";
+    c.fillStyle = "White";
+    if(status == 0) c.fillText("Game over!",220,200);
+    else c.fillText("You win!",230,200);
+    setTimeout(function(){ location.reload(); }, 3000);
+}
+
+
+function redraw(state) {
+    c.drawImage(backImage,0,0);
+
+
+    c.drawImage(playerImage, state.player.x, state.player.y);
+
+    for (a of state.aliens) {
+        for(b of state.bullets){
+            if(intersects(a, 60, 60, b, 10, 20)){
+                a.alive = false;
+                b.alive = false;
+                alive-=1;
+            }
+        }
+        if (a.alive) c.drawImage(alienImage, a.x, a.y);
+    }
+
+    for (b of state.bullets) {
+        if (!b.alive) bullets.splice(0, 1);
+        b.moveUp();
+        c.drawImage(rocketImage, b.x, b.y);
+    }
+}
+
+intersects = function (a, aw, ah, b, bw, bh) {
+    if(!a.alive) return false;
+    return !(b.x > (a.x + aw-1) ||
+        (b.x + bw) < a.x+1 ||
+        b.y > (a.y + ah) ||
+        (b.y + bh) < a.y);
+};
+
+class Alien {
     constructor(x, y) {
         this.x = x;
         this.y = y;
         this.alive = true;
     }
+}
 
-    test() {
-        alert("czesc");
+class Bullet {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.alive = true;
     }
-
+    moveUp() {
+        this.y = this.y - 20;
+        if (this.y <= 0) {
+            this.kill();
+        }
+    }
     kill() {
         this.alive = false;
     }
 }
 
-class bulet {
+class Player {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.visible = false;
-        this.active = false;
     }
-
-    mouveUp() {
-        this.y = this.y - 12;
-        this.visible = true;
-        if (this.y <= 0) {
-            this.kill();
-        }
-
+    moveLeft() {
+        if (this.x - 30 >= 0)
+            this.x = this.x - 30;
     }
-
-    start(x, y) {
-        this.visible = true;
-        this.active = true;
-        this.x = x;
-        this.y = y;
-    }
-
-    kill() {
-        this.visible = false;
+    moveRight() {
+        if (this.x + 30 <= 540) this.x = this.x + 30;
     }
 }
+
